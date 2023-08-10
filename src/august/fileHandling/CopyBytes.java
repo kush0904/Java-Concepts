c9fab78b3796d56bd643b14e9d3b7097934c43f8
kush0904 <kush904garg@gmail.com>
2023-08-10 21:44:16 +0530

    File Handling

---
+++ src/august/fileHandling/CopyBytes.java
@@ -0,0 +1,38 @@
+package august.fileHandling;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class CopyBytes {
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream in = null;
+        FileOutputStream out = null;
+
+        try {
+            out = new FileOutputStream("abc.txt");
+            in  = new FileInputStream("xyz.txt");
+
+            System.out.println("Total bytes available: " + in.available());
+
+
+            int c;
+            while ((c = in.read()) != -1) {
+                out.write(c);
+            }
+
+            System.out.println("The process is completed");
+        } catch (FileNotFoundException fe) {
+            System.out.println("Sorry, the source file is not found!");
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+        }
+    }
+}
